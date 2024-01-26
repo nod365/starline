26aef08172889cc98ee883297d390b8601cbd5e9
nod365 <97195148+nod365@users.noreply.github.com>
2024-01-26 10:51:24 +0300

    Update updates.js

--- updates.js
+++ updates.js
@@ -5,7 +5,7 @@ let product = {
 		support: 'https://t.me/deadnode',
 	},
 		MOTP : {
-		currentBuild: 283,
+		currentBuild: 284,
 		currentBuildReleaseDate: '2024 Jan 22',
 		support: 'https://t.me/deadnode',
 	}
