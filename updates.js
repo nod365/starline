9ac38c8a147e3ee7a0e19ec01c15bf2bb32ac434
nod365 <nod365@yandex.ru>
2025-02-13 19:59:11 +0300

    8

--- updates.js
+++ updates.js
@@ -10,7 +10,7 @@ let product = {
 		support: 'https://google.com',
 	},
 		CCPF : {
-		currentBuild: 7,
+		currentBuild: 8,
 		currentBuildReleaseDate: '13 февраля 2025',
 		support: 'https://google.com',
 	}
