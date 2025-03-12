f8962a3f1abb6aeb0e20ed33e889ba1f1395b72c
nod365 <nod365@yandex.ru>
2025-03-12 19:11:54 +0300

    Update vaporwave.js

--- TableParser/script/vaporwave.js
+++ TableParser/script/vaporwave.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     function generateArray() {
-        const GIFcount = 127
+        const GIFcount = 128
         const array = [];
         for (let i = 2; i <= GIFcount; i++) {
             array.push(i);
