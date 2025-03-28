bd9ba8de8a843dac4467f2a9dfbc41cb68c79c03
nod365 <nod365@yandex.ru>
2025-03-28 13:27:11 +0300

    327

--- updates.js
+++ updates.js
@@ -5,8 +5,8 @@ let product = {
 		support: 'https://google.com',
 	},
 		MOTP : {
-		currentBuild: 326,
-		currentBuildReleaseDate: '13 марта 2025',
+		currentBuild: 327,
+		currentBuildReleaseDate: '28 марта 2025',
 		support: 'https://google.com',
 	},
 		CCPF : {
