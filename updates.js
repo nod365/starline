2f8f9f28e8d4d0e46483d7ea7a6beafedc5103b1
nod365 <97195148+nod365@users.noreply.github.com>
2026-07-04 15:55:39 +0300

    2607041400

--- updates.js
+++ updates.js
@@ -7,8 +7,8 @@ let product = {
 		support: 'https://google.com',
 	},
 	MOTP: {
-		currentBuild: 2601071450,
-		currentBuildReleaseDate: '07 января 2026',
+		currentBuild: 2607041400,
+		currentBuildReleaseDate: '4 июля 2026',
 		support: 'https://google.com',
 	},
 	CCPF: {
